fb2de62d3f1a04a07babaa8da1b347287286e988
tianlong.xiang <tianlong.xiang@foxmail.com>
2022-11-20 14:56:01 +0800

    no: 没状态临时递交，检测当前关卡位置

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import time
 import win32gui
 import win32api
 import win32con
+import cv2
+import numpy as nm
+from datetime import datetime
 
 
 def stri_similar(s1, s2):
@@ -21,8 +24,8 @@ def find_chinese(file):
 
 
 def read_ans(file_name):
-    
-    with open(file_name, 'r', encoding="utf-8") as f:
+
+    with open('data/' + file_name, 'r', encoding="utf-8") as f:
         ans = []
         content = f.read()
         content = content.split("\n")
@@ -41,8 +44,24 @@ def read_ans(file_name):
                 print(e)
         return ans
 
+def left_click_position(hwd, x_position, y_position, sleep_time):
+    """鼠标左点击"""
+    # 将两个16位的值连接成一个32位的地址坐标
+    long_position = win32api.MAKELONG(x_position, y_position)
+    # 点击左键
+    win32api.SendMessage(hwd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, long_position)
+    win32api.SendMessage(hwd, win32con.WM_LBUTTONUP, win32con.MK_LBUTTON, long_position)
+    time.sleep(sleep_time)
+
+def get_mouse_position():
+    """获取鼠标位置"""
+    return win32api.GetCursorPos()
 
 if __name__ == '__main__':
+
+    #time.sleep(5)
+    #print(get_mouse_position())
+
     classname = "Chrome_WidgetWin_0"
     titlename = "咸鱼之王"
     # 获取句柄
@@ -50,18 +69,8 @@ if __name__ == '__main__':
     # 获取窗口左上角和右下角坐标
     left, top, right, bottom = win32gui.GetWindowRect(hwnd)
     print("识别到小程序位置：", left, top, right, bottom)
-    print(left+25, top+141, left+25 + 419, top+141 + 130)
 
     ans = read_ans('ans.txt')
-
-    # max = -1
-    # result = {}
-    # for item in ans:
-    #     ret = stri_similar('兄融夫人是《三国演义》虚构人8勿。', item['q'])
-    #     if ret > max:
-    #         max = ret
-    #         result = item
-    # print(result)
 
     reader = easyocr.Reader(['ch_sim', 'en'])
 
@@ -69,7 +78,33 @@ if __name__ == '__main__':
     question_str_old = ""
 
     while True:
-        img = ImageGrab.grab(bbox=(left+25, top+141, left+25 + 419, top+141 + 130))
+        img = ImageGrab.grab(
+            bbox=(left+25, top+141, left+25 + 419, top+141 + 130))
+        print(win32gui.ClientToScreen(hwnd, (204, 182)))
+        print(win32gui.ClientToScreen(hwnd, (398, 224)))
+        
+        game_level = 0
+        time_start = datetime.now()
+
+        img = ImageGrab.grab(bbox=(1971, 355, 2165, 397))
+        result = reader.readtext(cv2.cvtColor(nm.array(img), cv2.COLOR_BGR2GRAY))
+        if len(result) > 0:
+            game_level = result[0][1]
+            print("启动脚本，检测当前关卡：", game_level)
+
+        loop_count = 0
+        while True:
+            loop_count = loop_count + 1
+            if loop_count % 1000 == 0:
+                img = ImageGrab.grab(bbox=(1971, 355, 2165, 397))
+                result = reader.readtext(cv2.cvtColor(nm.array(img), cv2.COLOR_BGR2GRAY))
+                if len(result) > 0 and result[0][1] != game_level:
+                    game_level = result[0][1]
+                    time_consume = (datetime.now() - time_start)
+                    time_start = datetime.now()
+                    print("检测到关卡变动：{}, 耗时：{}".format(game_level, time_consume))
+
+            left_click_position(hwnd, 299, 783, 0.01)
 
         # print(img.mode)
         # print(range(img.width), range(img.height))
@@ -77,17 +112,17 @@ if __name__ == '__main__':
         for i in range(img.width):
             for j in range(img.height):
                 try:
-                    r,g,b = img.getpixel((i,j))
-                    color_set = [[48, 48, 48, 32],[82,31,26,40]]
+                    r, g, b = img.getpixel((i, j))
+                    color_set = [[48, 48, 48, 32], [82, 31, 26, 40]]
                     for color in color_set:
                         if color[0] - color[3] < r < color[0] + color[3] and color[1] - color[3] < g < color[1] + color[3] and color[2] - color[3] < b < color[2] + color[3]:
-                            img.putpixel((i,j), (0, 0, 0))
+                            img.putpixel((i, j), (0, 0, 0))
                             break
                         else:
-                            img.putpixel((i,j), (255, 255, 255))
+                            img.putpixel((i, j), (255, 255, 255))
                 except Exception as e:
                     print(e)
-                    continue 
+                    continue
 
         if img != img_old:
             img_old = img
@@ -120,16 +155,20 @@ if __name__ == '__main__':
 
                     print(question_str)
                     print(max, result, result['ans'])
-                    if(result['ans'] == '对'):
-                        win32api.SetCursorPos([left+166,top+930])
-                        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                    if (result['ans'] == '对'):
+                        win32api.SetCursorPos([left+166, top+930])
+                        win32api.mouse_event(
+                            win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
                         time.sleep(0.01)
-                        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                        win32api.mouse_event(
+                            win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
 
-                    if(result['ans'] == '错'):
-                        win32api.SetCursorPos([left+422,top+923])
-                        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                    if (result['ans'] == '错'):
+                        win32api.SetCursorPos([left+422, top+923])
+                        win32api.mouse_event(
+                            win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
                         time.sleep(0.01)
-                        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                        win32api.mouse_event(
+                            win32con.MOUSEEVENTF_LEFTUP | win32con.MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
 
             time.sleep(0.2)
