3bc64abd5c9928976f91c44ca0975d45e6dbca50
tianlong.xiang <tianlong.xiang@foxmail.com>
2022-11-25 23:21:06 +0800

    feat: task_auto_pass

--- xianyu.py
+++ xianyu.py
@@ -130,6 +130,41 @@ class xianyu:
             
             time.sleep(0.2)
 
+            
+    def task_auto_pass(self):
+        """自动过关"""
+
+        # 计算关卡位置
+        (x1, y1) = win32gui.ClientToScreen(self.hwnd, (204, 182))
+        (x2, y2) = win32gui.ClientToScreen(self.hwnd, (398, 224))
+
+        game_level = 0
+        time_start = datetime.now()
+
+        img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+        result = self.reader.readtext(cv2.cvtColor(
+            nm.array(img), cv2.COLOR_BGR2GRAY))
+        if len(result) > 0:
+            game_level = result[0][1]
+            logger.info("启动脚本，检测当前关卡：{}", game_level)
+
+        loop_count = 0
+        while True:
+            loop_count = loop_count + 1
+            if loop_count % 1000 == 0:
+                img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+                result = self.reader.readtext(cv2.cvtColor(
+                    nm.array(img), cv2.COLOR_BGR2GRAY))
+                if len(result) > 0 and result[0][1] != game_level:
+                    game_level = result[0][1]
+                    time_consume = (datetime.now() - time_start)
+                    time_start = datetime.now()
+                    logger.info("检测到关卡变动：{}, 耗时：{}", game_level, time_consume)
+
+            windows.left_click_position(self.hwnd, 299, 783, 0.01)
+
+
 if __name__ == '__main__':
     xianyu = xianyu()
+    xianyu.task_auto_pass()
     xianyu.task_auto_answer()
